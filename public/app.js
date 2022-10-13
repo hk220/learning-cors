7f6a924aeda2e2966a2732e9adecfca0ac980b5b
Kazuki Hara <kazuki.hara.dw3@gmail.com>
2022-10-14 08:22:55 +0900

    fetch data

--- public/app.js
+++ public/app.js
@@ -1,5 +1,32 @@
+// https://tarovlog.com/2021/03/10/react-uncaught-referenceerror-usestate-is-not-defined/
+const { useEffect, useState } = React
+
+function Body(props) {
+  const [ state, setState ] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("https://swapi.dev/api/people/1");
+      const data = await res.json();
+      setState(data);
+    }
+    fetchData();
+  });
+  
+  return (
+    <div>
+      <p>{state.name}</p>
+    </div>
+  );
+}
+
 function MyApp() {
-  return <h1>Hello, world!</h1>;
+  return (
+    <div>
+     <h1>Hello, world!</h1>
+     <Body />
+    </div>
+  );
 }
 
 const container = document.getElementById('root');
