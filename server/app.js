827088a91aee3c041b7ad09fded2b694971e7479
Kazuki Hara <kazuki.hara.dw3@gmail.com>
2022-10-14 09:40:05 +0900

    Use cors.

--- server/app.js
+++ server/app.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+if (process.env.USE_CORS) {
+  console.log('Attach CORS Header');
+  const cors = require('cors');
+  app.use(cors({
+    origin: 'http://localhost:8080',
+    credentials: true,
+    optionsSuccessStatus: 200
+  }));
+}
+
 app.get('/', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
   res.send('Hello World!');
 });
 
