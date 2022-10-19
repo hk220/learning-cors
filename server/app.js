8bc103c5e4cf88cea2b3626193601ae16020cb6a
Kazuki Hara <kazuki.hara.dw3@gmail.com>
2022-10-20 07:09:38 +0900

    Add cors option.

--- server/app.js
+++ server/app.js
@@ -1,18 +1,16 @@
+const { program } = require('commander');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-if (process.env.USE_CORS) {
-  console.log('Attach CORS Header');
-  const cors = require('cors');
-  app.use(cors({
-    origin: 'http://localhost:8080',
-    credentials: true,
-    optionsSuccessStatus: 200
-  }));
-}
+program.option('-c, --cors');
+program.parse();
+const options = program.opts();
+
+console.log(options);
 
 app.get('/', (req, res) => {
+  if (options.cors) res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.send('Hello World!');
 });
 
